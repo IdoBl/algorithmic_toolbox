f99dac28223d42f5c641c2cd745c140b43b6c31d
Ido Blutman <Ido_Blutman@amat.com>
2019-09-26 16:26:26 +0300

    Week 3 1st task

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -2,8 +2,24 @@
 import sys
 
 def get_change(m):
-    #write your code here
-    return m
+    if m < 10:
+        if m < 5:
+            # Can be changed into 1s only
+            return m
+        else:
+            # m >= 5
+            return m // 5 + m % 5
+    else:
+        # m >= 10
+        total_coins = 0
+        checked_types = [10, 5, 1]
+        for checked in checked_types:
+            type_coins = m // checked
+            total_coins += type_coins
+            m -= type_coins * checked
+            if m == 0:
+                return total_coins
+
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
