07da90d08d6bcbe2b029274a472c410861040178
Ido Blutman <Ido_Blutman@amat.com>
2019-10-27 14:22:23 +0200

    Week 4 3rd task: Quick sort with duplicate elements in O(nlogn)

--- week4_divide_and_conquer/3_improving_quicksort/sorting.py
+++ week4_divide_and_conquer/3_improving_quicksort/sorting.py
@@ -2,9 +2,29 @@
 import sys
 import random
 
+
 def partition3(a, l, r):
-    #write your code here
-    pass
+    # Partitioning using the dutch flag algorithm
+    # handle 2 elements array
+    if (r - l) <= 1:
+        if a[r] < a[l]:
+            # Swap elements
+            a[l], a[r] = a[r], a[l]
+            return l, r
+    x = a[l]  # x = lower position value
+    i = l
+    while i <= r:
+        if a[i] < x:
+            a[i], a[l] = a[l], a[i]
+            l += 1
+            i += 1
+        elif a[i] > x:
+            a[r], a[i] = a[i], a[r]
+            r -= 1
+        elif a[i] == x:
+            i += 1
+    return l, r
+
 
 def partition2(a, l, r):
     x = a[l]
@@ -22,10 +42,14 @@ def randomized_quick_sort(a, l, r):
         return
     k = random.randint(l, r)
     a[l], a[k] = a[k], a[l]
-    #use partition3
-    m = partition2(a, l, r)
-    randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
+    # use partition3
+    m1, m2 = partition3(a, l, r)
+    randomized_quick_sort(a, l, m1 - 1)
+    randomized_quick_sort(a, m2 + 1, r)
+    # #use partition2
+    # m = partition2(a, l, r)
+    # randomized_quick_sort(a, l, m - 1)
+    # randomized_quick_sort(a, m + 1, r)
 
 
 if __name__ == '__main__':
