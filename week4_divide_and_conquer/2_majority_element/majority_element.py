9278f4777b09bc4b1324b9c87541486217290967
Ido Blutman <Ido_Blutman@amat.com>
2019-10-27 09:53:55 +0200

    Week 4 2nd task: Finding majority element using Moore’s Voting Algorithm

--- week4_divide_and_conquer/2_majority_element/majority_element.py
+++ week4_divide_and_conquer/2_majority_element/majority_element.py
@@ -1,18 +1,35 @@
 # Uses python3
 import sys
 
-def get_majority_element(a, left, right):
-    if left == right:
-        return -1
-    if left + 1 == right:
-        return a[left]
-    #write your code here
-    return -1
+
+def find_candidate():
+    maj_index = 0
+    count = 1
+    for i in range(1, len(a)):
+        if a[maj_index] == a[i]:
+            count += 1
+        else:
+            count -= 1
+        if count == 0:
+            maj_index = i
+            count += 1
+
+    return a[maj_index]
+
+
+def find_majority():
+    c = find_candidate()
+    count = 0
+    for i in range(len(a)):
+        if a[i] == c:
+            count += 1
+    return 1 if count > n//2 else 0
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
+    if find_majority():
         print(1)
     else:
         print(0)
