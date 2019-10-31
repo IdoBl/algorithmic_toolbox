586e53854dfb5321ceebf3435a810bbb49f406a1
Ido Blutman <Ido_Blutman@amat.com>
2019-10-31 17:31:46 +0200

    Week 3: add tasks 6 and 7 solutions

--- week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
+++ week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
@@ -3,8 +3,20 @@ import sys
 
 def optimal_summands(n):
     summands = []
-    #write your code here
+    # write your code here
+    i = 1
+    sum_summands = 0
+    while sum_summands <= n:
+        if (sum_summands + i) <= n:
+            summands.append(i)
+            sum_summands += i
+        else:
+            summands[-1] += n - sum_summands
+            break
+        i = i + 1
+
     return summands
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
--- week3_greedy_algorithms/7_maximum_salary/largest_number.py
+++ week3_greedy_algorithms/7_maximum_salary/largest_number.py
@@ -1,17 +1,31 @@
-#Uses python3
+# Uses python3
 
 import sys
 
-def largest_number(a):
-    #write your code here
+
+def is_greater(digit, max_digit):
+    return digit + max_digit > max_digit + digit
+
+
+def largest_number(digits):
+    # write your code here
     res = ""
-    for x in a:
-        res += x
+
+    while len(digits) != 0:
+        max_digit = '0'
+        for digit in digits:
+            # print("digit str ", digit_str)
+            if is_greater(digit, max_digit):
+                max_digit = digit
+                # print("max", max_digit)
+        res += max_digit
+        # print(max_digit)
+        digits.remove(max_digit)
     return res
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = input.split()
-    a = data[1:]
-    print(largest_number(a))
-    
+    digits = data[1:]
+    print(largest_number(digits))
